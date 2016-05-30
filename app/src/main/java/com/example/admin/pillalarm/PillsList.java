19d4bb93206b534c415185d28f3a5d1e0cba4bab
Farah Arabi <farabi@uci.edu>
2016-05-30 00:37:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/admin/pillalarm/PillsList.java
@@ -0,0 +1,15 @@
+package com.example.admin.pillalarm;
+
+/**
+ * Created by admin on 5/29/2016.
+ */
+public enum PillsList {
+    Choice1(R.string.choice1),
+    Choice2(R.string.choice2),
+    Choice3(R.string.choice3),
+    Choice4(R.string.choice4);
+public int title;
+    PillsList(int title){
+        this.title=title;
+    }
+}
